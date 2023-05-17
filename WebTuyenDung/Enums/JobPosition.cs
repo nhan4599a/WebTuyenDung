3e5063292eabd9833eff76f42a7c507f0739d4db
nhan <nhan0385790927@gmail.com>
2023-05-17 09:04:57 +0700

    update code

--- WebTuyenDung/Enums/JobPosition.cs
+++ WebTuyenDung/Enums/JobPosition.cs
@@ -1,4 +1,6 @@
-﻿namespace WebTuyenDung.Enums
+﻿using System;
+
+namespace WebTuyenDung.Enums
 {
     public enum JobPosition : byte
     {
@@ -11,5 +13,25 @@
         PM,
         ITSupport,
         Designer
+    }
+
+    public static class JobPositionRepresentation
+    {
+        public static string GetRepresentation(this JobPosition jobPosition)
+        {
+            return jobPosition switch
+            {
+                JobPosition.BA => "Business Analysist",
+                JobPosition.BrSE => "Bridge Engineering",
+                JobPosition.Developer => "Developer",
+                JobPosition.Tester => "Tester",
+                JobPosition.QC => "Quality Control",
+                JobPosition.QA => "Quality Assurance",
+                JobPosition.PM => "Quản trị dự án",
+                JobPosition.ITSupport => "IT Support, IT Helpdesk",
+                JobPosition.Designer => "Designer",
+                _ => throw new NotImplementedException()
+            };
+        }
     }
 }
